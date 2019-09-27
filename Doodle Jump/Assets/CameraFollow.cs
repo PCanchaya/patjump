84beee8c9badf583838598d35c62e7cb06ab58c9
alex <allinproinc@gmail.com>
2019-09-27 22:09:26 +0100

    Initial commit

---
+++ Doodle Jump/Assets/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+	public Transform target;
+
+    // Start is called before the first frame update
+    void LateUpdate () {
+    	if (target.position.y > transform.position.y)
+    	{
+    		Vector3 newPos = new Vector3(transform.position.x, target.position.y, transform.position.z);
+    		transform.position = newPos;
+    	}
+        
+    }
+
+  
+}
